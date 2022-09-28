80bd2502076ec4a19ff97f8d552aa79d42251c31
Robert Rodgers <rodgers.rob@gmail.com>
2022-09-28 21:58:58 +0000

    Updated basic exercises string and arrays folders

---
+++ ruby_basics_exercises/arrays/free_the_lizard.rb
@@ -0,0 +1,8 @@
+pets = ['cat', 'dog', 'fish', 'lizard']
+my_pets = pets[2..3]
+
+#Remove 'lizard' from my_pets then print the value of my_pets.
+
+my_pets.pop
+
+puts "I have a #{my_pets[0]}."
---
+++ ruby_basics_exercises/strings/pluralize.rb
@@ -0,0 +1,19 @@
+words = 'car human elephant airplane'
+
+=begin
+array = words.split(" ")
+
+array.each do |x|
+  puts x + 's'
+end
+=end
+
+words = 'car human elephant airplane'
+
+words.split(" ").each do |words|
+  puts words + 's'
+end
+
+
+
+
